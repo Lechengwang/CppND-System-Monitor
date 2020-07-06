c0e3bf2f43f9953608ceb53865df7e144ae154fd
Lecheng Wang <sebaoyu@gmail.com>
2020-07-06 21:15:54 +0000

    Done format.h and format.cpp

--- include/format.h
+++ include/format.h
@@ -4,6 +4,9 @@
 #include <string>
 
 namespace Format {
+int const M2S = 60;
+int const H2M = 60;
+int const H2S = H2M * M2S;
 std::string ElapsedTime(long times);  // TODO: See src/format.cpp
 };                                    // namespace Format
 
--- src/format.cpp
+++ src/format.cpp
@@ -8,4 +8,10 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) { 
+  long hh = seconds / Format::H2S;
+  long reminder = seconds - Format::H2S * hh;
+  long mm = reminder / Format::M2S;
+  long ss = reminder - Format::M2S * mm;
+  return std::to_string(hh) + ":" + std::to_string(mm) + ":" + std::to_string(ss);
+}
